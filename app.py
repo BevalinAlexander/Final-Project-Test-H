e9e24df1bf5ea711007677117645e310407ac2fe
Bevalin Alexander <Balexan1@sgu.edu>
2021-09-11 08:52:19 -0400

    updated files

--- app.py
+++ app.py
@@ -2,41 +2,33 @@
 from flask import Flask, render_template
 
 # create instance of Flask app
-app = Flask(
-    __name__,
-    template_folder="templates"
-)
-
-
-# # Set variables
-# name = "Aaron"
-# hobby = "Baseball"
+app = Flask(__name__)
 
 
 # create route that renders index.html template
 @app.route("/")
-def index():
+def home():
 
     return render_template("index.html")
 
+# create route that renders dct.html
+@app.route("/dct")
+def dct():
+
+    return render_template("dct.html")
+
 
 # create route that renders NavieBayes.html
-@app.route("/NaiveBayes")
-def NaiveBayes():
+@app.route("/naivebayes")
+def naivebayes():
 
-    return render_template("NaiveBayes.html")
+    return render_template("naivebayes.html")
 
-# create route that renders tableau.html
+# # create route that renders tableau.html
 @app.route("/tableau")
 def tableau():
 
     return render_template("tableau.html")
-
-# # create route that renders dct.html
-# @app.route("/dct")
-# def tableau():
-
-#     return render_template("dct.html")
 
 
 if __name__ == "__main__":
